c065918d745caf77ea72d1c3815c13e7a513b917
DiegoDiazVi <ddiazvi29075@universidadean.edu.co>
2024-07-18 17:17:28 -0500

    feat: Update AddUserForm import and wrap with UserProvider

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,15 @@
-import AddUserForm from "./components/AddUserForm";
+import AddUserForm from "./components/AddUserForm/AddUserForm.jsx";
+import UserProvider from "./context/UsersContext";
 
 function App() {
   return (
     <main>
       <h1>Gestión de usuarios</h1>
-      <AddUserForm/>
+      <UserProvider>
+        <AddUserForm/>
+      </UserProvider>
     </main>
   )
 }
-
-const initialUsers = [
-  {
-    id: 1,
-    name: "Alice Johnson",
-    email: "alice.johnson@example.com"
-  },
-  {
-    id: 2,
-    name: "Bob Smith",
-    email: "bob.smith@example.com"
-  },
-  {
-    id: 3,
-    name: "Carol Williams",
-    email: "carol.williams@example.com"
-  }
-];
 
 export default App

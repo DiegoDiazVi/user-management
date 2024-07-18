c55356a24c0784980f1c2acd51984f3ab8384aff
DiegoDiazVi <ddiazvi29075@universidadean.edu.co>
2024-07-18 17:56:34 -0500

    feat: Update App.jsx to include UserList component and import App.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import AddUserForm from "./components/AddUserForm/AddUserForm.jsx";
+import UserList from "./components/UserList/UserList.jsx";
 import UserProvider from "./context/UsersContext";
+import './App.css'
 
 function App() {
   return (
@@ -7,6 +9,7 @@ function App() {
       <h1>Gestión de usuarios</h1>
       <UserProvider>
         <AddUserForm/>
+        <UserList/>
       </UserProvider>
     </main>
   )

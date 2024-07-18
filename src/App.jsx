f665be48b88477710ff5dc1abbd53c64835f010d
DiegoDiazVi <ddiazvi29075@universidadean.edu.co>
2024-07-18 16:44:21 -0500

    feat: Add AddUserForm component and initial user data

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,30 @@
-import { useState } from 'react'
+import AddUserForm from "./components/AddUserForm";
+
 function App() {
   return (
-    <>
-    </>
+    <main>
+      <h1>Gestión de usuarios</h1>
+      <AddUserForm/>
+    </main>
   )
 }
+
+const initialUsers = [
+  {
+    id: 1,
+    name: "Alice Johnson",
+    email: "alice.johnson@example.com"
+  },
+  {
+    id: 2,
+    name: "Bob Smith",
+    email: "bob.smith@example.com"
+  },
+  {
+    id: 3,
+    name: "Carol Williams",
+    email: "carol.williams@example.com"
+  }
+];
 
 export default App
